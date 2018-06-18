3584797a5a0213de0ca0776e0aa4fee13a327a1d
zondahuman <zondahuman@gmail.com>
2018-06-18 21:47:56 +0800

    add

---
+++ spike-flash/src/main/resources/queue/distribute.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lee
+--
+
+local totalLen = redis.call('llen', KEYS[1]) --查询队列的长度
+
+if totalLen >= 5 then
+    return 0
+end
+
+local currentLen = redis.call('lpush', KEYS[1], ARGV[1])
+
+if currentLen >= 5 then
+    return 0
+end
+
+return 1
