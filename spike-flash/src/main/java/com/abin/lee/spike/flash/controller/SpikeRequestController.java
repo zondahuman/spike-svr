d161c4bb6358339f783008a2e4a5da7339765152
zondahuman <zondahuman@gmail.com>
2018-07-03 11:44:52 +0800

    add

---
+++ spike-flash/src/main/java/com/abin/lee/spike/flash/controller/SpikeRequestController.java
@@ -0,0 +1,46 @@
+package com.abin.lee.spike.flash.controller;
+
+import com.abin.lee.spike.common.JsonUtil;
+import com.abin.lee.spike.flash.model.SpikeModel;
+import com.abin.lee.spike.flash.service.DistributeLockService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by abin on 2018/6/17.
+ * https://www.hifreud.com/2017/01/18/zookeeper-16-java-api-curator-07-queue/#distributed-queue%E5%88%86%E5%B8%83%E5%BC%8F%E9%98%9F%E5%88%97
+ */
+
+@Controller
+@RequestMapping("/spikeRequest")
+@Slf4j
+public class SpikeRequestController {
+
+
+    @PatchMapping(value = "/spikePatch")
+    @ResponseBody
+    public String spikePatch(SpikeModel spikeModel) {
+        String result = "FAILURE";
+        try {
+            System.out.println("-----------------------------------------------");
+            System.out.println("spikeModel----" + JsonUtil.toJson(spikeModel));
+            System.out.println("-----------------------------------------------");
+            result = "SUCCESS";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+
+
+
+
+}
--- spike-flash/src/main/java/com/abin/lee/spike/flash/model/SpikeModel.java
+++ spike-flash/src/main/java/com/abin/lee/spike/flash/model/SpikeModel.java
@@ -1,10 +1,16 @@
 package com.abin.lee.spike.flash.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by abin on 2018/6/13.
  */
+@Setter
+@Getter
 public class SpikeModel {
-
+    private int id;
+    private String userName;
 
 
 
